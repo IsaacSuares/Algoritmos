59d274076a60abe563d65b928feabd962e39dee1
IsaacSuares <112450363+IsaacSuares@users.noreply.github.com>
2023-09-22 14:10:25 -0300

    Remove size function

--- Insertion-Sort.c
+++ Insertion-Sort.c
@@ -1,16 +1,8 @@
 //Insertion-Sort
 #include<stdio.h>
 
-int size(int vetor[]){
-	int cont = 0;
-	while(vetor[cont]){
-		cont++;
-	}
-	return cont - 3;
-}
-
-void printer(int vetor[]){
-    for(int i = 0; i < size(vetor); i++){
+void printer(int vetor[], int size){
+    for(int i = 0; i < size; i++){
 	    printf("%d ", vetor[i]);
 	}
 	printf("\n \n");
@@ -20,12 +12,13 @@ int main()
 {
     /*Printing before sorting*/
 	int vetor[10] = {10, 8, 7, 5, 4, 6, 2, 3, 1, 9};
+	int size = (int) (sizeof(vetor) / sizeof(vetor[0]));
 	printf("** Before Sorting **\n");
-	printer(vetor);
+	printer(vetor, size);
 	
 	/*INSERTION SORT*/
 	int key, before;
-	for(int i = 1; i < size(vetor); i++){
+	for(int i = 1; i < size; i++){
 	    key = vetor[i];
 	    before = i - 1;
 	    while(before > -1 && vetor[before] > key){
@@ -37,7 +30,7 @@ int main()
 	
 	/*Printing after sorting*/
 	printf("** After Sorting **\n");
-	printer(vetor);
+	printer(vetor, size);
 	
 	return 0;
 }
